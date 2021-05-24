97373259ed4485d0229f5bb8a559a20d3b386620
Alican Demirtas <demirtasdev@hotmail.com>
2021-05-24 19:02:16 +0100

    IsSorted is moved into the Miscellaneous project.

---
+++ AlgorithmsFoundations/Miscellaneous/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using SearchingData;
+
+namespace Miscellaneous
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var unordered = new List<int> {6, 20, 8, 19, 56, 23, 87, 41, 49, 53};
+            var ordered = new List<int> {6, 8, 19, 20, 23, 41, 49, 53, 56, 87};
+            IsSorted.Run(unordered);
+            IsSorted.Run(ordered);
+        }
+    }
+}
--- AlgorithmsFoundations/SearchingData/Program.cs
+++ AlgorithmsFoundations/SearchingData/Program.cs
@@ -7,15 +7,11 @@ namespace SearchingData
     {
         static void Main(string[] args)
         {
-            int result;
-
-            var unordered = new List<int> {6, 20, 8, 19, 56, 23, 87, 41, 49, 53};
-
+            // var unordered = new List<int> {6, 20, 8, 19, 56, 23, 87, 41, 49, 53};
             // LinearSearch.Run(87, unordered);
             // LinearSearch.Run(141, unordered);
 
             var ordered = new List<int> {6, 8, 19, 20, 23, 41, 49, 53, 56, 87};
-
             BinarySearch.Run(23, ordered);
             BinarySearch.Run(87, ordered);
             BinarySearch.Run(250, ordered);
